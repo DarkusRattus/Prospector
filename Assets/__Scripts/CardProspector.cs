998f45ad1000488d8cc3368ab075f427df0eacfd
DarkusRattus <kristian.benson@rutgers.edu>
2016-11-10 19:56:55 -0500

    Updates

---
+++ Assets/__Scripts/CardProspector.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// This is an enum, which defines a type of variable tht only has a few possible named values
+// The CardState variable type has one of four values: drawpile, tableau, target, & discard
+public enum CardState
+{
+    drawpile,
+    tableau,
+    target,
+    discard
+}
+
+public class CardProspector : Card {
+     
+    // This is how you use the enum CardState
+    public CardState state = CardState.drawpile;
+    // This hiddenBy list stores wich other cards will keep this one face down
+    public List<CardProspector> hiddenBy = new List<CardProspector>();
+    // LayoutID matches this card to a Layout XML ID if it's a tableau card
+    public int layoutID;
+    // The SlowDef class stores information pulled in from they LayoutXML <slot>
+    public SlotDef slotDef;
+
+}
