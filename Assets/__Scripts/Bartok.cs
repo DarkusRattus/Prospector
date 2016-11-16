8e55e65190f1651bd5b91e45f7f30a325bd75fcc
DarkusRattus <kristian.benson@rutgers.edu>
2016-11-16 12:14:10 -0500

    Added Bartok script

--- Assets/__Scripts/Bartok.cs
+++ Assets/__Scripts/Bartok.cs
@@ -1,15 +1,32 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Bartok : MonoBehaviour {
 
-	// Use this for initialization
+    static public Bartok S;
+
+    public TextAsset deckXML;
+    public TextAsset layoutXML;
+    public Vector3 layoutCenter = Vector3.zero;
+
+    public bool __________________;
+
+    public Deck deck;
+    public List<CardBartok> drawPile;
+    public List<CardBartok> discardPile;
+
+    void Awake()
+    {
+        S = this;
+    }
+
 	void Start () {
-	
+        deck = GetComponent<Deck>();        // Get the Deck
+        deck.InitDeck(deckXML.text);        // Pass the DeckXML to it
+        Deck.Shuffle(ref deck.cards);       // This shuffles the deck
+        // The ref keyword passes a reference to deck.cards, which allows deck.cards to be modified by Deck.Shuffle()
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
